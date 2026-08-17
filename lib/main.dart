9edd19f204a25ef0269ba2b3de0d2504e6a81b51
a.valadan <amirvaladan1993@gmail.com>
2026-08-17 12:17:39 +0330

    fix initialize

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'core/theme/app_theme.dart';
 import 'core/theme/theme_provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const ProviderScope(child: BarchasbApp()));
 }
 
